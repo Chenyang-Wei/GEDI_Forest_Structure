066c96629c1d04c0e60e35f54dd225c5b239b0af
Chenyang Wei <ChenyangWei.CWei@gmail.com>
2024-02-20 15:08:25 -0500

    Download the GIS data for the eBird Best Practice.

--- R/Scripts/eBird_Best_Practices/Ch.2_eBird Data.R
+++ R/Scripts/eBird_Best_Practices/Ch.2_eBird Data.R
@@ -1,11 +1,102 @@
 ## Source: https://ebird.github.io/ebird-best-practices/ebird.html
-## Last updated: 1/31/2024.
+## Last updated: 2/20/2024.
 
 
-###### Chapter 2: eBird Data ######
+###### Chapter 1: Introduction and Setup ######
+
+# 1.4 Setup ---------------------------------------------------------------
+
+## 1.4.3 R packages
+
+if (!requireNamespace("remotes", quietly = TRUE)) {
+  install.packages("remotes")
+}
+
+remotes::install_github("ebird/ebird-best-practices")
+
+
+## 1.4.5 GIS data
+
+library(dplyr)
+library(rnaturalearth)
+library(sf)
 
 setwd("C:/Research_Projects/Bird/eBird")
 
+# File to save spatial data.
+gpkg_file <- "data/gis-data.gpkg"
+
+dir.create(dirname(gpkg_file),
+           showWarnings = FALSE,
+           recursive = TRUE)
+
+# Political boundaries (land border with lakes removed).
+ne_land <- ne_download(
+  scale = 50,
+  category = "cultural",
+  type = "admin_0_countries_lakes",
+  returnclass = "sf") |> 
+  filter(CONTINENT %in% c("North America", "South America")) |> 
+  st_set_precision(1e6) |> 
+  st_union()
+
+# Country boundaries.
+ne_countries <- ne_download(
+  scale = 50,
+  category = "cultural",
+  type = "admin_0_countries_lakes",
+  returnclass = "sf") |> 
+  select(country = ADMIN, country_code = ISO_A2)
+
+# State boundaries for the United States.
+ne_states <- ne_download(
+  scale = 50,
+  category = "cultural",
+  type = "admin_1_states_provinces",
+  returnclass = "sf") |> 
+  filter(iso_a2 == "US") |> 
+  select(state = name, state_code = iso_3166_2)
+
+# Country lines.
+#   Download the global result and then filter to North America with
+#   st_intersect().
+ne_country_lines <- ne_download(
+  scale = 50,
+  category = "cultural",
+  type = "admin_0_boundary_lines_land",
+  returnclass = "sf") |> 
+  st_geometry()
+
+lines_on_land <- st_intersects(
+  ne_country_lines,
+  ne_land,
+  sparse = FALSE
+) |> 
+  as.logical()
+
+ne_country_lines <- ne_country_lines[lines_on_land]
+
+# State lines in North America.
+ne_state_lines <- ne_download(
+  scale = 50,
+  category = "cultural",
+  type = "admin_1_states_provinces_lines",
+  returnclass = "sf"
+) |> 
+  filter(ADM0_A3 %in% c("USA", "CAN")) |> 
+  mutate(iso_a2 = recode(ADM0_A3, USA = "US", CAN = "CAN")) |> 
+  select(country = ADM0_NAME, country_code = iso_a2)
+
+# Save all layers to a geopackage.
+unlink(gpkg_file)
+write_sf(ne_land, gpkg_file, "ne_land")
+write_sf(ne_countries, gpkg_file, "ne_countries")
+write_sf(ne_states, gpkg_file, "ne_states")
+write_sf(ne_country_lines, gpkg_file, "ne_country_lines")
+write_sf(ne_state_lines, gpkg_file, "ne_state_lines")
+
+
+###### Chapter 2: eBird Data ######
 
 # 2.4 Importing eBird data into R -----------------------------------------
 
